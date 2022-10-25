2b5e7e988bb27fee9643a0bb5ec17da4a709e30b
PiloniA <piloni@live.at>
2022-10-26 01:14:46 +0200

    added Selenium steps

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+
+namespace selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FirefoxBinary binary = new FirefoxBinary();
+            FirefoxOptions options = new FirefoxOptions();//optional
+            options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            IWebDriver driver = new FirefoxDriver(@"C:\tools\Webdrivers", options);
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Id("W0wltc")).Click();
+            driver.FindElement(By.Name("q")).SendKeys("Test Automation"+ Keys.Return);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+    }
+}
